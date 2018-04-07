356eaa94bbdbfa5f239e86ce8432dd42f65640e1
brianpark627 <park.brianjh@gmail.com>
2018-04-07 14:59:38 -0400

    authorization

---
+++ authorize.js
@@ -0,0 +1,49 @@
+var SpotifyWebApi = require ('spotify-web-api-node');
+var opn = require ('opn')
+
+//scope array holds possible scope parameters
+const state = 'YXJyyvYXlzc3RhcnRhdDA='
+	_scope = ['user-read-private', 'playlist-read-private', 'playlist-read-collaborative', 'user-read-currently-playing', 'streaming', 'user-read-playback-state']
+	
+var spotifyApi = new SpotifyWebApi({
+	clientId :  'acd0f18a3e124101af31f9b3582130c6',
+	clientSecret : '276a4580f7e94dd1a20f5d797b95dbba',
+	redirectUri : 'https://www.google.com/',
+	response_type : 'code'
+})
+
+var authURL = spotifyApi.createAuthorizeURL(_scope, state, true)
+
+console.log(authURL)
+opn(authURL)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
