56d3184dbc17941e02efd613a324c74fcabe6e98
Vishal Patel <vpatel14@terpmail.umd.edu>
2018-12-27 15:14:07 -0500

    run_file.js works with mocha or node

--- run_file.js
+++ run_file.js
@@ -1,15 +1,29 @@
 const cp = require('child_process');
 const path = require('path');
 
-let file = process.argv.slice(2)[0];
-file = path.resolve(file);
+let args = process.argv.slice(2);
 
+let file = args[0];
+file = path.resolve(file);
 if (!file) {
     console.error("file wasn't specified");
     process.exit(1);
 }
 
-let cmd = `node --inspect-brk "${file}"`;
+let useMocha = args[1] || 'false';
+
+if (useMocha !== 'false' && useMocha !== 'true') {
+    console.error('invalid useMocha value')
+    process.exit(1);
+}
+
+let cmd = 'node --inspect-brk ';
+
+if (useMocha === 'true') {
+    cmd += './node_modules/mocha/bin/mocha ';
+}
+
+cmd += `"${file}"`;
 
 let subproc = cp.exec(cmd);
 
