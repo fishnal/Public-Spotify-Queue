cc403b03130753cc52c5eacb01027c1e65215a04
Vishal Patel <vpatel14@terpmail.umd.edu>
2018-12-29 15:59:11 -0500

    moving CLA parsing to index.js, can use .conf files
    CLA parsing should be in the file that the user executes, which is index.js. Currently, CLA parsing is in src/server.js, which isn't good practice because it'll complain if it sees arguments that it shouldn't, when really index.js should be the one complaining about this. Working on stripping CLA parsing entirely from src/server.js
    Can use configuration files (ideally .conf, but doesn't really matter, just needs to follow the pattern <option>=<value>) instead of passing CLAs yourself each time
    Removed run_file.js, kind of pointless

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-const server = require('./src/server.js');
+const commandArgs = require('commander');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
 
-server.start();
+commandArgs.version('0.1.0', '-v|--version')
+    .option('-c|--config <config file>', 'Configuration file that specifies any required and optional arguments. Overrides all other arguments')
+    .option('-i|--id <client id>', 'Spotify Developer Application client ID', /^[a-zA-Z0-9]+$/)
+    .option('-s|--secret <client secret>', 'Spotify Developer Application client secret', /^[a-zA-Z0-9]+$/)
+    .option('-a|--address <server address>', 'Address that server is hosted on', 'http://localhost')
+    .option('-p|--port [port number]', 'Port for the server', /^[0-9]+$/, 3000)
+    .parse(process.argv);
+
+(async() => {
+    if (commandArgs.config) {
+        // read in config file's contents
+        let configContents = await util.promisify(fs.readFile)(path.resolve(commandArgs.config));
+        // split into lines
+        configContents = configContents.toString().split('\n');
+        let new_options = { };
+        let optionKeys = ['id', 'secret', 'address', 'port'];
+
+        // parse the config lines
+        configContents.forEach((line) => {
+            let match = /(?<option>id|secret|address|port)=(?<value>.*)/.exec(line);
+
+            if (match) {
+                new_options[match.groups.option] = match.groups.value === '' ? null : match.groups.value;
+            }
+        });
+
+        process.argv.splice(2);
+        Object.keys(new_options).forEach((key) => {
+            process.argv.push(`--${key}`);
+            process.argv.push(new_options[key]);
+        });
+    }
+
+    const server = require('./src/server.js');
+    server.start();
+})();
